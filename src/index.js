dd0d74ea526771ca56a5903186e27dfbe7ffbdc4
LondiweSa <140651868+LondiweSa@users.noreply.github.com>
2024-04-05 23:59:28 +0200

    final js update for api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = documennt.querySelector("temperature");
+  let temperature = response.data.temperature.current;
+  let appCityElement = document.querySelector("#app-city");
+
+  appCityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "394eo39f5f82bbfb4e70ed9a04294tea";
+
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let appCityElement = document.querySelector("#app-city");
 
-  appCityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
